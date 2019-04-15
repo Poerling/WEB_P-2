c79f604542026e739ae85e069ffb53c06b30bfd2
Poerling <44144795+Poerling@users.noreply.github.com>
2019-04-15 08:37:40 +0200

    cookiesbox added
    cookiesbox added

--- cookiebox.js
+++ cookiebox.js
@@ -1,9 +1,40 @@
-function setzeCookie() {
-    var datum = new Date();
-    var jetzigeZeit = datum.getTime();
-    var gueltigBis = jetzigeZeit + 86400;
-    datum.setTime(gueltigBis);
-    var EnddatumString = datum.toUTCString();
-    document.cookie = "Minziro = set;expires=" + EnddatumString;
-    document.getElementById("cookieHinweis").style.display = "none";
+'use strict';
+
+function init() {
+    var besuch = zaehlerstand();
+    var ausgabe = document.getElementById('info');
+    ausgabe.innerHTML = besuch;
 }
+
+function wertHolen() {
+    var Wert = "";
+    if (document.cookie) {
+        var Wertstart = document.cookie.indexOf("=") + 1;
+        var Wertende = document.cookie.indexOf(";");
+        if (Wertende == -1) {
+            Wertende = document.cookie.length;
+        }
+        Wert = document.cookie.substring(Wertstart, Wertende);
+    }
+    return Wert;
+}
+
+function wertSetzen(Bezeichner, Wert, Verfall) {
+    var jetzt = new Date();
+    var Auszeit = new Date(jetzt.getTime() + Verfall);
+    document.cookie = Bezeichner + "=" + Wert + "; expires=" + Auszeit.toGMTString() +
+        ";";
+}
+
+function zaehlerstand() {
+    var Verfallszeit = 1000 * 60 * 60 * 24 * 365;
+    var Anzahl = wertHolen();
+    var Zaehler = 0;
+    if (Anzahl != "") {
+        Zaehler = parseInt(Anzahl) || 0;
+    }
+    Zaehler = Zaehler + 1;
+    wertSetzen("Zaehler", Zaehler, Verfallszeit);
+    return (Zaehler);
+}
+window.addEventListener('DOMContentLoaded', init);
